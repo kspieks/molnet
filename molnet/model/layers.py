2e1d98783ba94dc0af6deaaaa78c2a583b2e49b9
Kevin Spiekermann <kspieker@mit.edu>
2022-08-06 08:57:03 -0400

    Add DMPNN layer

---
+++ molnet/model/layers.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch_geometric.nn import MessagePassing
+
+
+class DMPNNConv(MessagePassing):
+    def __init__(self, args):
+        super(DMPNNConv, self).__init__(aggr='add')
+        self.lin = nn.Linear(args.hidden_size, args.hidden_size)
+        self.mlp = nn.Sequential(nn.Linear(args.hidden_size, args.hidden_size),
+                                 nn.BatchNorm1d(args.hidden_size),
+                                 nn.ReLU())
+
+    def forward(self, x, edge_index, edge_attr):
+        row, col = edge_index
+        a_message = self.propagate(edge_index, x=None, edge_attr=edge_attr)
+
+        rev_message = torch.flip(edge_attr.view(edge_attr.size(0) // 2, 2, -1), dims=[1]).view(edge_attr.size(0), -1)
+        return a_message, self.mlp(a_message[row] - rev_message)
+
+    def message(self, x_j, edge_attr):
+        return F.relu(self.lin(edge_attr))
