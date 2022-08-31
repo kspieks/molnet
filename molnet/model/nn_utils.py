f232a416a3f179f00ed7221c952747f917cb58ca
Kevin Spiekermann <kspieker@mit.edu>
2022-08-30 20:44:57 -0400

    Make pytorch be fully reproducible
    https://pytorch.org/docs/stable/notes/randomness.html

--- molnet/model/nn_utils.py
+++ molnet/model/nn_utils.py
@@ -151,7 +151,10 @@ def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def param_count(model: nn.Module) -> int:
